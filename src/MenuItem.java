b7b6197aa6d85d204a54e659a5a13a768b204be7
timchang27 <64303732+timchang27@users.noreply.github.com>
2021-11-05 17:37:35 +0800

    Add files via upload

---
+++ src/MenuItem.java
@@ -0,0 +1,27 @@
+public class MenuItem {
+
+    String name;
+    String description;
+    double price;
+
+    public MenuItem(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+
+
+}
